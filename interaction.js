66e103b156b7186ecdccbcf724a80b42b20d5539
IsraelBlack <israelblack98@outlook.com>
2022-04-27 06:41:48 +1200

    Whoa

---
+++ interaction.js
@@ -0,0 +1,33 @@
+/*#############
+Catergory Modal
+##############*/
+
+// Get the modal
+var catModal = document.getElementById("catModal");
+
+// Get the button that opens the modal
+var catBtn = document.getElementById("catButton");
+
+// Get the <span> element that closes the modal
+var catSpan = document.getElementsByClassName("catergoryClose")[0];
+
+// When the user clicks on the button, open the modal
+catBtn.onclick = function() {
+  catModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+catSpan.onclick = function() {
+    catModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == catModal) {
+    catModal.style.display = "none";
+  }
+} 
+
+/*#############
+Shopping Cart
+##############*/
